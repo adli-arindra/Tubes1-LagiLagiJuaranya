c5260abc7f545542656b500ae1728528c66f1945
Adli Arindra <adliarindra27@gmail.com>
2025-03-17 16:12:59 +0700

    feat: added bot templates

---
+++ src/alternative-bots/bot3/bot3.cs
@@ -0,0 +1,35 @@
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+
+public class bot3 : Bot
+{
+    static void Main(string[] args)
+    {
+        new bot3().Start();
+    }
+
+    bot3() : base(BotInfo.FromFile("bot3.json")) { }
+
+    public override void Run()
+    {
+        while (IsRunning)
+        {
+            Forward(100);
+            TurnGunRight(360);
+            Back(100);
+            TurnGunRight(360);
+        }
+    }
+
+    public override void OnScannedBot(ScannedBotEvent evt)
+    {
+        Fire(1);
+    }
+
+    public override void OnHitByBullet(HitByBulletEvent evt)
+    {
+        var bearing = CalcBearing(evt.Bullet.Direction);
+
+        TurnLeft(90 - bearing);
+    }
+}
